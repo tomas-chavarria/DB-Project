b8c50bcaefbd0b7b483e636ce86e5ae5869fb94c
tomas-chavarria <chavarriatomas05@gmail.com>
2026-04-17 14:28:37 -0400

    Fix comments for db_handler, implement close_connection

--- db_handler.py
+++ db_handler.py
@@ -15,95 +15,75 @@ conn = connect(user=DB_CONFIG["username"], password=DB_CONFIG["password"], host=
 cur = conn.cursor()
 
 
+# new_item - An Item object containing a new item to be inserted into the DB in the item table.
+# new_item and its attributes will never be None.
 def add_item(new_item: Item = None):
-    """
-    new_item - An Item object containing a new item to be inserted into the DB in the item table.
-        new_item and its attributes will never be None.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# new_customer - A Customer object containing a new customer to be inserted into the DB in the customer table. 
+# new_customer and its attributes will never be None.
 def add_customer(new_customer: Customer = None):
-    """
-    new_customer - A Customer object containing a new customer to be inserted into the DB in the customer table.
-        new_customer and its attributes will never be None.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# original_customer_id - A string containing the customer id for the customer to be edited.
+# new_customer - A Customer object containing attributes to update. If an attribute is None, it should not be altered.
 def edit_customer(original_customer_id: str = None, new_customer: Customer = None):
-    """
-    original_customer_id - A string containing the customer id for the customer to be edited.
-    new_customer - A Customer object containing attributes to update. If an attribute is None, it should not be altered.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# item_id - A string containing the Item ID for the item being rented.
+# customer_id - A string containing the customer id of the customer renting the item.
 def rent_item(item_id: str = None, customer_id: str = None):
-    """
-    item_id - A string containing the Item ID for the item being rented.
-    customer_id - A string containing the customer id of the customer renting the item.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# Returns the customer's new place in line.
 def waitlist_customer(item_id: str = None, customer_id: str = None) -> int:
-    """
-    Returns the customer's new place in line.
-    """
     raise NotImplementedError("you must implement this function")
 
+
+# Removes person at position 1 and shifts everyone else down by 1.
 def update_waitlist(item_id: str = None):
-    """
-    Removes person at position 1 and shifts everyone else down by 1.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# Moves a rental from rental to rental_history with return_date = today.
 def return_item(item_id: str = None, customer_id: str = None):
-    """
-    Moves a rental from rental to rental_history with return_date = today.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# Adds 14 days to the due_date.
 def grant_extension(item_id: str = None, customer_id: str = None):
-    """
-    Adds 14 days to the due_date.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# Returns a list of Item objects matching the filters.
 def get_filtered_items(filter_attributes: Item = None,
                        use_patterns: bool = False,
                        min_price: float = -1,
                        max_price: float = -1,
                        min_start_year: int = -1,
                        max_start_year: int = -1) -> list[Item]:
-    """
-    Returns a list of Item objects matching the filters.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# Returns a list of Customer objects matching the filters.
 def get_filtered_customers(filter_attributes: Customer = None, use_patterns: bool = False) -> list[Customer]:
-    """
-    Returns a list of Customer objects matching the filters.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# Returns a list of Rental objects matching the filters.
 def get_filtered_rentals(filter_attributes: Rental = None,
                          min_rental_date: str = None,
                          max_rental_date: str = None,
                          min_due_date: str = None,
                          max_due_date: str = None) -> list[Rental]:
-    """
-    Returns a list of Rental objects matching the filters.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# Returns a list of RentalHistory objects matching the filters.
 def get_filtered_rental_histories(filter_attributes: RentalHistory = None,
                                   min_rental_date: str = None,
                                   max_rental_date: str = None,
@@ -111,52 +91,43 @@ def get_filtered_rental_histories(filter_attributes: RentalHistory = None,
                                   max_due_date: str = None,
                                   min_return_date: str = None,
                                   max_return_date: str = None) -> list[RentalHistory]:
-    """
-    Returns a list of RentalHistory objects matching the filters.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# Returns a list of Waitlist objects matching the filters.
 def get_filtered_waitlist(filter_attributes: Waitlist = None,
                           min_place_in_line: int = -1,
                           max_place_in_line: int = -1) -> list[Waitlist]:
-    """
-    Returns a list of Waitlist objects matching the filters.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# Returns num_owned - active rentals. Returns -1 if item doesn't exist.
 def number_in_stock(item_id: str = None) -> int:
-    """
-    Returns num_owned - active rentals. Returns -1 if item doesn't exist.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# Returns the customer's place_in_line, or -1 if not on waitlist.
 def place_in_line(item_id: str = None, customer_id: str = None) -> int:
-    """
-    Returns the customer's place_in_line, or -1 if not on waitlist.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# Returns how many people are on the waitlist for this item.
 def line_length(item_id: str = None) -> int:
-    """
-    Returns how many people are on the waitlist for this item.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# Commits all changes made to the db.
 def save_changes():
-    """
-    Commits all changes made to the db.
-    """
     raise NotImplementedError("you must implement this function")
 
 
+# IMPLEMENTED
+# Closes the cursor and connection.
 def close_connection():
-    """
-    Closes the cursor and connection.
-    """
-    raise NotImplementedError("you must implement this function")
+    if cur:
+            cur.close()
+            print("Cursor closed.")
+    if conn:
+        conn.close()
+        print("Connection closed.")
 
